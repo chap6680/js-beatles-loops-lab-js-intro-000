f99ce6b11be8b67842af8031fc95a72ecd03429e
David Chapman <chap6680misc@gmail.com>
2018-01-18 12:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   let temp = [];
   for (let mcount = 0; mcount < musicians.length; mcount++) {
-          temp[mcount]= musicians[mcount] + " plays the " + instruments[mcount]
+          temp[mcount]= musicians[mcount] + " plays " + instruments[mcount]
   }
 
   return temp;
